6ee8b8a21673e2dd6cb01662a94d5ec0e5687cbf
egavrisco <egavrisco@pentalog.com>
2019-05-30 15:02:41 +0300

    B-18 moved the code from ProjectController in ProjectHandler

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Project;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Services\ProjectHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,25 +12,27 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProjectController extends AbstractController
 {
+    private $handler;
+
+    public function __construct(ProjectHandler $handler)
+    {
+        $this->handler = $handler;
+    }
+
     /**
      * @Route("/api/projects", name="project_add", methods={"POST"})
      */
-    public function addAction(Request $request, EntityManagerInterface $em): JsonResponse
+    public function addAction(Request $request): JsonResponse
     {
+
         $data = \json_decode($request->getContent(), true);
 
-        $repository = $em->getRepository(Project::class);
-        $existing = $repository->findOneBy(['name' => $data['name']]);
-        if ($existing) {
+        $isValid = $this->handler->validate($data);
+        if (!$isValid) {
             return new JsonResponse(null, Response::HTTP_BAD_REQUEST);
         }
-        if (empty($data['name'])) {
-            return new JsonResponse(null, Response::HTTP_BAD_REQUEST);
-        }
-        $project = new Project();
-        $project->setName($data['name']);
-        $em->persist($project);
-        $em->flush();
+
+        $project = $this->handler->save($data);
 
         return new JsonResponse($project->getId());
     }
@@ -38,49 +40,39 @@ class ProjectController extends AbstractController
     /**
      * @Route("/api/projects/{project}", name="project_edit", methods={"POST"})
      */
-    public function editAction(Request $request, EntityManagerInterface $em, Project $project): JsonResponse
+    public function editAction(Request $request, Project $project): JsonResponse
     {
         $data = \json_decode($request->getContent(), true);
-        $repository = $em->getRepository(Project::class);
-        $existing = $repository->findOneBy(['name' => $data['name']]);
-        if ($existing && $existing->getId() !== $project->getId()) {
+
+        $isValid = $this->handler->validate($data, $project);
+        if ($isValid) {
+            $project = $this->handler->save($data, $project);
+        }
+
+        if ($isValid === false) {
             return new JsonResponse(null, Response::HTTP_BAD_REQUEST);
         }
-        if (empty($data['name'])) {
-            return new JsonResponse(null, Response::HTTP_BAD_REQUEST);
-        }
-        $project->setName($data['name']);
-        $em->persist($project);
-        $em->flush();
 
         return new JsonResponse($project->getId());
+
     }
 
     /**
      * @Route("/api/projects", name="project_list", methods={"GET"})
      */
-    public function listAction(EntityManagerInterface $em): JsonResponse
+    public function listAction(): JsonResponse
     {
-        $repository = $em->getRepository(Project::class);
-        $projects = $repository->findAll();
-        $arr = [];
-        foreach ($projects as $proj) {
-            $arr [] = [
-                'id' => $proj->getId(),
-                'name' => $proj->getName(),
-            ];
-        }
+        $list = $this->handler->getList();
 
-        return new JsonResponse($arr);
+        return new JsonResponse($list);
     }
 
     /**
      * @Route("/api/projects/{project}", name="project_delete", methods={"DELETE"})
      */
-    public function deleteAction(EntityManagerInterface $em, Project $project): JsonResponse
+    public function deleteAction(Project $project): JsonResponse
     {
-        $em->remove($project);
-        $em->flush();
+        $this->handler->delete($project);
 
         return new JsonResponse(null, Response::HTTP_OK);
     }
---
+++ src/Services/ProjectHandler.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Project;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ProjectHandler
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function validate($data, ?Project $project = null): bool
+    {
+        $repository = $this->em->getRepository(Project::class);
+        $existing = $repository->findOneBy(['name' => $data['name']]);
+        if ($existing) {
+            return false;
+        }
+        if (empty($data['name'])) {
+            return false;
+        }
+
+        if ($project !== null && $existing && $existing->getId() !== $project->getId()) {
+            return false;
+
+        }
+
+        return true;
+    }
+
+    public function save($data, ?Project $project = null): Project
+    {
+        if ($project === null) {
+            $project = new Project();
+        }
+
+        $project->setName($data['name']);
+        $this->em->persist($project);
+        $this->em->flush();
+
+        return $project;
+    }
+
+    public function getList(): array
+    {
+        $repository = $this->em->getRepository(Project::class);
+        $projects = $repository->findAll();
+        $arr = [];
+        foreach ($projects as $proj) {
+            $arr [] = [
+                'id' => $proj->getId(),
+                'name' => $proj->getName(),
+            ];
+        }
+
+        return $arr;
+    }
+
+    public function delete(Project $project): void
+    {
+        $this->em->remove($project);
+        $this->em->flush();
+    }
+
+}
